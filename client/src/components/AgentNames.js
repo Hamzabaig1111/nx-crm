14ab1c0d9c4c3a9baf0beaa990cb8e371efdf921
hamzabaig1111@gmail.com <hamzabaig1111@gmail.com>
2024-01-08 10:54:52 +0500

    Update AgentNames

--- client/src/components/AgentNames.js
+++ client/src/components/AgentNames.js
@@ -1,12 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
-import { axios } from "../components/BaseURL";
-
+import axios from 'axios'
 export const GetAgentNamesDirect = () => {
   const { isPending, error, data, refetch } = useQuery({
     queryKey: ["agentsnamesData"],
     queryFn: () =>
       axios
-        .get("/agents/getagentsnames", {
+        .get("https://nexskill-server.vercel.app/api/agents/getagentsnames", {
           withCredentials: true,
         })
         .then((res) => {
